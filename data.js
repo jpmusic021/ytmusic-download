766a026868551e36ab8dfc80ed2baf1c94b5e370
021jayu <jjayuyt@gmail.com>
2025-07-18 11:30:33 +0530

    data.js
    Signed-off-by: 021jayu <jjayuyt@gmail.com>

--- data.js
+++ data.js
@@ -1,13 +1,29 @@
-let songs = [
+// data.js
+
+const songs = [
   {
-    id: "Vm_Kib6xceE",
-    title: "Gulabi Sadi Chill Out Mix",
-    category: "Chill"
+    id: "DTYbfiPeQHI",
+    title: "Chhri rang rangili Remix kamlesh barot",
+    category: "Remix Chill out",
+    type: "youtube"
   },
   {
-    id: "k-7Riia-oqI",
-    title: "Paruna Behi Gya Tara Aangane",
-    category: "Timli"
+    id: "1vYafZ_Pb7Y",
+    title: "DJ Jungle Tribal Beat",
+    category: "Adivasi",
+    type: "youtube"
+  },
+  {
+    id: "https://drive.google.com/uc?id=1K1AbC_XYZ123&export=download",
+    title: "Garba 2024 Hit",
+    category: "Gujarati",
+    type: "gdrive"
+  },
+  {
+    id: "https://drive.google.com/uc?id=1LxFv_example_45&export=download",
+    title: "Bhajan - Jay Mataji",
+    category: "Devotional",
+    type: "gdrive"
   }
 ];
 
