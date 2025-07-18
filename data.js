e6a88c3ba687c25c077039cf5f51eb79a51992b7
021jayu <jjayuyt@gmail.com>
2025-07-18 11:49:19 +0530

    data.js
    Signed-off-by: 021jayu <jjayuyt@gmail.com>

--- data.js
+++ data.js
@@ -1,30 +1,46 @@
-// data.js
+// data.js - Firebase version
 
-const songs = [
-  {
-    id: "DTYbfiPeQHI",
-    title: "Chhri rang rangili Remix kamlesh barot",
-    category: "Remix Chill out",
-    type: "youtube"
-  },
-  {
-    id: "1vYafZ_Pb7Y",
-    title: "DJ Jungle Tribal Beat",
-    category: "Adivasi",
-    type: "youtube"
-  },
-  {
-    id: "https://drive.google.com/uc?id=1K1AbC_XYZ123&export=download",
-    title: "Garba 2024 Hit",
-    category: "Gujarati",
-    type: "gdrive"
-  },
-  {
-    id: "https://drive.google.com/uc?id=1LxFv_example_45&export=download",
-    title: "Bhajan - Jay Mataji",
-    category: "Devotional",
-    type: "gdrive"
-  }
-];
+// Firebase Config - Replace with your Firebase project configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
 
-localStorage.setItem("songs", JSON.stringify(songs));
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// Save song to Firebase
+function saveSongToFirebase(song) {
+  const songRef = database.ref("songs");
+  songRef.push(song);
+}
+
+// Load all songs from Firebase
+function loadSongsFromFirebase(callback) {
+  const songRef = database.ref("songs");
+  songRef.once("value", snapshot => {
+    const songs = [];
+    snapshot.forEach(childSnapshot => {
+      const data = childSnapshot.val();
+      songs.push({ ...data, key: childSnapshot.key });
+    });
+    callback(songs);
+  });
+}
+
+// Delete song from Firebase
+function deleteSongFromFirebase(songKey, callback) {
+  const songRef = database.ref("songs/" + songKey);
+  songRef.remove()
+    .then(() => {
+      console.log("Song deleted");
+      if (callback) callback();
+    })
+    .catch(error => console.error("Delete error:", error));
+}
