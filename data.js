04972ef9b9edca20849250fa4466e949a11db840
021jayu <jjayuyt@gmail.com>
2025-07-18 11:02:50 +0530

    data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+  {
+    id: "Vm_Kib6xceE",
+    title: "Gulabi Sadi Chill Out Mix",
+    category: "Chill"
+  },
+  {
+    id: "k-7Riia-oqI",
+    title: "Paruna Behi Gya Tara Aangane",
+    category: "Timli"
+  }
+];
+
+localStorage.setItem("songs", JSON.stringify(songs));
